0ce54aa6a9614656c0744ef4b12406fe70e25b24
Jake Rosenberg <jakemr1996@gmail.com>
2024-06-24 23:37:24 -0400

    added septa map to assets, started svg component for reponsive map

---
+++ ui/src/components/SeptaMap.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <svg>
+    <!-- Add the svg stuff here for the responsive map, see https://github.com/svg-->
+    </svg>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
